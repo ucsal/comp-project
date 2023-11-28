87ba96426c147e655f1b89e89371fd8c15260ba8
CandidoRPNeto <candido.neto@ucsal.edu.br>
2023-11-28 11:53:11 -0300

    arquivo de texto como entrada

--- src/App.java
+++ src/App.java
@@ -1,44 +1,82 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App extends JFrame {
 
-    private JTextArea areaDeCodigo;
-    private JButton botaoEnviar;
+    private JTextArea areaDeTexto;
+    private JButton botaoSelecionarArquivo;
 
     public App() {
-        setTitle("Inserção de Código");
+        // Configurações da janela
+        setTitle("Leitura de Arquivo");
         setSize(500, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        areaDeCodigo = new JTextArea(80, 80);
-        botaoEnviar = new JButton("Enviar");
-        botaoEnviar.addActionListener(e -> enviarCodigo());
+        // Criação dos componentes
+        areaDeTexto = new JTextArea(10, 40);
+        areaDeTexto.setEditable(false); // Impede a edição do texto
+        JScrollPane scrollPane = new JScrollPane(areaDeTexto);
+        botaoSelecionarArquivo = new JButton("Selecionar Arquivo");
+        botaoSelecionarArquivo.addActionListener(e -> selecionarArquivo());
 
+        // Configuração do layout
         setLayout(new BorderLayout());
 
-        add(new JScrollPane(areaDeCodigo), BorderLayout.CENTER);
-        add(botaoEnviar, BorderLayout.SOUTH);
+        // Adição dos componentes à janela
+        add(scrollPane, BorderLayout.CENTER);
+        add(botaoSelecionarArquivo, BorderLayout.SOUTH);
     }
 
-    private void enviarCodigo() {
-        /*
-         * 
-         * 
-         * Metodos de compilação e tradução
-         * 
-         * 
-         */
-        mostrarMensagem(areaDeCodigo.getText());
+    private void selecionarArquivo() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Arquivos .232", "232");
+        fileChooser.setFileFilter(filter);
+        int resultado = fileChooser.showOpenDialog(this);
+
+        if (resultado == JFileChooser.APPROVE_OPTION) {
+            try {
+                String caminhoDoArquivo = fileChooser.getSelectedFile().getAbsolutePath();
+
+                /*
+                 * 
+                 * É aqui que pegamos as linhas para o compilador
+                 * 
+                 */
+                String[] linhas = lerArquivo(caminhoDoArquivo);
+
+                areaDeTexto.setText("");
+                for (String linha : linhas) {
+                    areaDeTexto.append(linha + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(this, "Erro ao ler o arquivo.", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
-    private void mostrarMensagem(String mensagem) {
-        JTextArea areaDeTextoDialog = new JTextArea(mensagem);
-        areaDeTextoDialog.setEditable(false);
+    private String[] lerArquivo(String caminhoDoArquivo) throws IOException {
+        List<String> linhas = new ArrayList<>();
 
-        JOptionPane.showMessageDialog(this, new JScrollPane(areaDeTextoDialog), "Código Inserido",
-                JOptionPane.INFORMATION_MESSAGE);
+        try (BufferedReader leitor = new BufferedReader(new FileReader(caminhoDoArquivo))) {
+            String linha;
+            while ((linha = leitor.readLine()) != null) {
+                linhas.add(linha);
+            }
+        }
+
+        // Converter a lista para um array de strings
+        return linhas.toArray(new String[0]);
     }
 
     public static void main(String[] args) {
