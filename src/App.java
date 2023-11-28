a8c52641787a72d58a3b1724cb28128f90080248
CandidoRPNeto <candido.neto@ucsal.edu.br>
2023-11-28 12:19:57 -0300

    Fase de input e outputbasicos finalizados

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,25 +9,27 @@ public class App extends JFrame {
 
     private JTextArea areaDeTexto;
     private JButton botaoSelecionarArquivo;
+    private String cabecalho = "Código da equipe: 04" + "\n" +
+            "Componentes:" + "\n" +
+            "       Cândido Ribeiro Peralva Neto;candido.neto@ucsal.edu.br;(74)988484843" + "\n" +
+            "       Davi Brito de Moura Pires;davi.pires@ucsal.edu.br;(71) 9134-9388" + "\n" +
+            "       SAMIR DANTAS;samir.barreto@ucsal.edu.br;(71) 8650-0010" + "\n" +
+            "       Yllo Luis;yllo.jesus@ucsal.edu.br;(71) 8129-0020";
 
     public App() {
-        // Configurações da janela
         setTitle("Leitura de Arquivo");
         setSize(500, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        // Criação dos componentes
         areaDeTexto = new JTextArea(10, 40);
         areaDeTexto.setEditable(false); // Impede a edição do texto
         JScrollPane scrollPane = new JScrollPane(areaDeTexto);
         botaoSelecionarArquivo = new JButton("Selecionar Arquivo");
         botaoSelecionarArquivo.addActionListener(e -> selecionarArquivo());
 
-        // Configuração do layout
         setLayout(new BorderLayout());
 
-        // Adição dos componentes à janela
         add(scrollPane, BorderLayout.CENTER);
         add(botaoSelecionarArquivo, BorderLayout.SOUTH);
     }
@@ -38,26 +37,40 @@ public class App extends JFrame {
     private void selecionarArquivo() {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Arquivos .232", "232");
         fileChooser.setFileFilter(filter);
+
         int resultado = fileChooser.showOpenDialog(this);
 
         if (resultado == JFileChooser.APPROVE_OPTION) {
             try {
-                String caminhoDoArquivo = fileChooser.getSelectedFile().getAbsolutePath();
+                File arquivoSelecionado = fileChooser.getSelectedFile();
+                String caminhoDoArquivo232 = arquivoSelecionado.getAbsolutePath();
+                String nomeDoArquivoSemExtensao = getNomeDoArquivoSemExtensao(arquivoSelecionado);
+                String diretorioDoArquivo = arquivoSelecionado.getParent();
 
-                /*
+                /**
                  * 
-                 * É aqui que pegamos as linhas para o compilador
+                 * Linhas do arquivo original onde vamos trabalhar
                  * 
                  */
-                String[] linhas = lerArquivo(caminhoDoArquivo);
+                String[] linhas = lerArquivo(caminhoDoArquivo232);
 
                 areaDeTexto.setText("");
                 for (String linha : linhas) {
                     areaDeTexto.append(linha + "\n");
                 }
+
+                // Parte do compilador que vai fazer o TAB e devolver um array de linhas
+                String caminhoArquivoTAB = diretorioDoArquivo + File.separator + nomeDoArquivoSemExtensao + ".TAB";
+                salvarComoArquivo(linhas, caminhoArquivoTAB);
+
+                // Parte do compilador que vai fazer o LEX e devolver um array de linhas
+                String caminhoArquivoLEX = diretorioDoArquivo + File.separator + nomeDoArquivoSemExtensao + ".LEX";
+                salvarComoArquivo(linhas, caminhoArquivoLEX);
+
+                JOptionPane.showMessageDialog(this,
+                        "Arquivos salvos em:\n" + caminhoArquivoTAB + "\n" + caminhoArquivoLEX);
             } catch (IOException e) {
                 e.printStackTrace();
                 JOptionPane.showMessageDialog(this, "Erro ao ler o arquivo.", "Erro", JOptionPane.ERROR_MESSAGE);
@@ -75,8 +88,31 @@ public class App extends JFrame {
             }
         }
 
-        // Converter a lista para um array de strings
         return linhas.toArray(new String[0]);
+    }
+
+    private void salvarComoArquivo(String[] linhas, String caminhoDoArquivo) {
+
+        try (PrintWriter escritor = new PrintWriter(new FileWriter(caminhoDoArquivo))) {
+            escritor.println(cabecalho + "\n");
+            for (String linha : linhas) {
+                escritor.println(linha);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Erro ao salvar o arquivo.", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private String getNomeDoArquivoSemExtensao(File arquivo) {
+        String nomeDoArquivo = arquivo.getName();
+        int posicaoPonto = nomeDoArquivo.lastIndexOf('.');
+
+        if (posicaoPonto > 0 && posicaoPonto < nomeDoArquivo.length() - 1) {
+            return nomeDoArquivo.substring(0, posicaoPonto);
+        }
+
+        return nomeDoArquivo;
     }
 
     public static void main(String[] args) {
